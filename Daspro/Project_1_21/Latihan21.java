b252ae94d047b4c80aa5ae2442b5cbd01e4bde9e
rahman936 <rahmannnn23450@gmail.com>
2026-08-28 18:40:26 +0700

    1

---
+++ Daspro/Project_1_21/Latihan21.java
@@ -0,0 +1,8 @@
+public class Latihan21 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Muhammad Danish Athaurrahman");
+        // System.out.print("Belajar Java itu mudah!");
+        System.out.println("Belajar java mudah");
+        System.out.println("Semakin sering mencoba semakin jago");
+    }
+}
---
+++ Daspro/Project_1_21/Tugas21.java
@@ -0,0 +1,13 @@
+public class Tugas21 {
+    public static void main(String[] args) {
+        // Langkah 1
+        System.out.println("Setelah Langkah 1: Nampan A = kosong, Nampan B = bulan, Nampan C = bintang");
+        // Langkah 2
+        System.out.println("Setelah Langkah 2: Nampan A = bulan, Nampan B = kosong, Nampan C = bintang");
+        // Langkah 3
+        System.out.println("Setelah Langkah 3: Nampan A = bulan, Nampan B = bintang, Nampan C = kosong");
+        // Kesimpulan
+        System.out.println(
+                "Kesimpulan: Pernyataan yang benar adalah \"(a) Kedua bola sudah bertukar tempat dan (e) nampan C kosong\"");
+    }
+}
